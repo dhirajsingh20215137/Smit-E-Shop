0e6a5efd518b9d9b872d1a28ccf4c33c64e0c84f
ujjainsingh88 <85781890+ujjainsingh88@users.noreply.github.com>
2023-05-03 00:42:36 +0530

    addings

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import {registerController,loginController, testController} from '../controller/authController.js'
-import { requireSignIn } from '../middlewares/authMiddleware.js'
+import { isAdmin, requireSignIn } from '../middlewares/authMiddleware.js'
 
 // router object 
 const router = express.Router()
@@ -13,6 +13,6 @@ router.post('/register', registerController)
 router.post('/login', loginController)
 
 //test routes
-router.get('/test',requireSignIn, testController)
+router.get('/test',requireSignIn,isAdmin, testController)
 
 export default router
